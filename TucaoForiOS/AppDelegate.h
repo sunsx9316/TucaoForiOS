7af1e857ae8e66b0000c2bf75893113febde6823
sunsx9316 <sunsx9316@gmail.com>
2016-08-23 11:47:13 +0800

    Initial Commit

---
+++ TucaoForiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TucaoForiOS
+//
+//  Created by JimHuang on 16/8/23.
+//  Copyright © 2016年 jimHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TucaoForiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TucaoForiOS
+//
+//  Created by JimHuang on 16/8/23.
+//  Copyright © 2016年 jimHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
